614c34a809ff454e6e9253b625426fcd12cac198
Brian Starkey <stark3y@gmail.com>
2018-12-28 18:00:20 +0000

    Initial implementation
    We can scroll through images and apply an algorithm to them

--- main.go
+++ main.go
@@ -5,13 +5,23 @@ import (
 	"fmt"
 	"flag"
 	"image"
+	"image/color"
+	_ "image/png"
+	_ "image/gif"
+	_ "image/jpeg"
+	"math"
+	"os"
 	"time"
 
 	"github.com/ungerik/go-cairo"
 	"github.com/veandco/go-sdl2/sdl"
+
+	"github.com/usedbytes/mini_mouse/ui/widget"
 )
 
 var bench bool
+var left *widget.ImageWidget
+var right *widget.ImageWidget
 
 func init() {
 	gob.Register(&image.NRGBA{})
@@ -20,10 +30,175 @@ func init() {
 	const (
 		defaultBench = false
 		usageBench   = "Measure drawing time"
-
 	)
 
 	flag.BoolVar(&bench, "b", defaultBench, usageBench)
+}
+
+func absdiff_uint8(a, b uint8) int {
+	if a < b {
+		return int(b - a)
+	} else {
+		return int(a - b)
+	}
+}
+
+func deltaC(a, b color.NRGBA) uint8 {
+	deltaR := float64(absdiff_uint8(a.R, b.R))
+	deltaG := float64(absdiff_uint8(a.G, b.G))
+	deltaB := float64(absdiff_uint8(a.B, b.B))
+
+	deltaC := math.Sqrt( (2 * deltaR * deltaR) +
+			    (4 * deltaG * deltaG) +
+			    (3 * deltaB * deltaB) +
+			    (deltaR * ((deltaR * deltaR) - (deltaB * deltaB)) / 256.0))
+
+	return uint8(deltaC)
+}
+
+func rgb(in color.RGBA) color.NRGBA {
+	rNon := uint8(float64(in.R) * 255.0 / float64(in.A))
+	gNon := uint8(float64(in.G) * 255.0 / float64(in.A))
+	bNon := uint8(float64(in.B) * 255.0 / float64(in.A))
+
+	return color.NRGBA{rNon, gNon, bNon, 0xff}
+}
+
+func absdiff(a, b color.Color) color.Gray {
+	switch aPix := a.(type) {
+	case color.NRGBA:
+		bPix := b.(color.NRGBA)
+		diff := absdiff_uint8(aPix.R, bPix.R) +
+			absdiff_uint8(aPix.G, bPix.G) +
+			absdiff_uint8(aPix.B, bPix.B)
+		return color.Gray{uint8(float64(diff) / (3))}
+	case color.RGBA:
+		bPix := b.(color.RGBA)
+
+		aNon := uint8(float64(aPix.R) * 255.0 / float64(aPix.A))
+		bNon := uint8(float64(bPix.R) * 255.0 / float64(bPix.A))
+		diff := absdiff_uint8(aNon, bNon)
+
+		aNon = uint8(float64(aPix.G) * 255.0 / float64(aPix.A))
+		bNon = uint8(float64(bPix.G) * 255.0 / float64(bPix.A))
+		diff += absdiff_uint8(aNon, bNon)
+
+		aNon = uint8(float64(aPix.B) * 255.0 / float64(aPix.A))
+		bNon = uint8(float64(bPix.B) * 255.0 / float64(bPix.A))
+		diff += absdiff_uint8(aNon, bNon)
+
+		return color.Gray{uint8(float64(diff) / (3))}
+	case color.Gray:
+		bPix := b.(color.Gray)
+		return color.Gray{uint8(absdiff_uint8(aPix.Y, bPix.Y))}
+	}
+
+	return color.Gray{0}
+}
+
+func wikidiff(a, b color.Color) color.Gray {
+	switch aPix := a.(type) {
+	case color.NRGBA:
+		bPix := b.(color.NRGBA)
+
+		return color.Gray{deltaC(aPix, bPix)}
+	case color.RGBA:
+		bPix := b.(color.RGBA)
+
+		return color.Gray{deltaC(rgb(aPix), rgb(bPix))}
+	case color.Gray:
+		bPix := b.(color.Gray)
+		diff := absdiff_uint8(aPix.Y, bPix.Y)
+		return color.Gray{uint8(diff)}
+	}
+
+	return color.Gray{0}
+}
+
+func diffImage(in image.Image) *image.Gray {
+	w, h := in.Bounds().Dx(), in.Bounds().Dy()
+
+	out := image.NewGray(image.Rect(0, 0, w, h - 1))
+	for x := 0; x < w; x++ {
+		for y := 0; y < h - 1; y++ {
+			diff := wikidiff(in.At(x, y), in.At(x, y + 1))
+			//diff := absdiff(in.At(x, y), in.At(x, y + 1))
+			out.Set(x, y, diff)
+		}
+	}
+
+	return out
+}
+
+func findMinMaxColwise(img *image.Gray) []image.Point {
+	ret := make([]image.Point, img.Bounds().Dx())
+	w, h := img.Bounds().Dx(), img.Bounds().Dy()
+
+	for i := 0; i < w; i++ {
+		var min, max uint8 = 255, 0
+		for j := 0; j < h; j++ {
+			pix := img.At(i, j).(color.Gray)
+			if pix.Y < min {
+				min = pix.Y
+			}
+			if pix.Y > max {
+				max = pix.Y
+			}
+		}
+		ret[i] = image.Pt(int(max), int(min))
+	}
+
+	return ret
+}
+
+func expandContrastColWise(img *image.Gray, minMax []image.Point) {
+	w, h := img.Bounds().Dx(), img.Bounds().Dy()
+
+	for i := 0; i < w; i++ {
+		scale := 255.0 / float32(minMax[i].X - minMax[i].Y)
+
+		for j := 0; j < h; j++ {
+			pix := img.At(i, j).(color.Gray)
+			newVal := float32(pix.Y - uint8(minMax[i].Y)) * scale
+			img.Set(i, j, color.Gray{uint8(newVal)})
+		}
+	}
+}
+
+func threshold(img *image.Gray) {
+	w, h := img.Bounds().Dx(), img.Bounds().Dy()
+
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			pix := img.At(j, i).(color.Gray)
+			if pix.Y >= 128 {
+				img.Set(j, i, color.Gray{255})
+			} else {
+				img.Set(j, i, color.Gray{0})
+			}
+		}
+	}
+}
+
+func updateImage(fname string) {
+	inFile, err := os.Open(fname)
+	if err != nil {
+		panic(err)
+	}
+
+	img, _, err := image.Decode(inFile)
+	if err != nil {
+		panic(err)
+	}
+
+	left.SetImage(img)
+
+	grad := diffImage(img)
+	minMax := findMinMaxColwise(grad)
+	expandContrastColWise(grad, minMax)
+	threshold(grad)
+
+	right.SetImage(grad)
 }
 
 func main() {
@@ -51,6 +226,11 @@ func main() {
 
 	cairoSurface := cairo.NewSurfaceFromData(sdlSurface.Data(), cairo.FORMAT_ARGB32, int(sdlSurface.W), int(sdlSurface.H), int(sdlSurface.Pitch));
 
+	left = widget.NewImageWidget()
+	right = widget.NewImageWidget()
+
+	idx := 0
+	updateImage(flag.Arg(idx))
 
 	running := true
 	tick := time.NewTicker(16 * time.Millisecond)
@@ -63,11 +243,35 @@ func main() {
 				running = false
 				break
 			case *sdl.KeyboardEvent:
-				_ = ev
+				if ev.State == 0 {
+					if ev.Keysym.Sym == sdl.K_LEFT {
+						idx -= 1
+						if idx < 0 {
+							idx = flag.NArg() - 1
+						}
+					}
+
+					if ev.Keysym.Sym == sdl.K_RIGHT {
+						idx += 1
+						if idx >= flag.NArg() {
+							idx = 0
+						}
+					}
+
+					updateImage(flag.Arg(idx))
+				}
 			}
 		}
 
 		now := time.Now()
+
+		cairoSurface.Save()
+		left.Draw(cairoSurface, image.Rect(50, 50, 550, 550))
+		cairoSurface.Restore()
+
+		cairoSurface.Save()
+		right.Draw(cairoSurface, image.Rect(600, 50, 1100, 550))
+		cairoSurface.Restore()
 
 		// Finally draw to the screen
 		cairoSurface.Flush()
