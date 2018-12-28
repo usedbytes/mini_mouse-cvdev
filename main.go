470bac861c4aa1ab4a6a92bb133bbe501bf588c5
Brian Starkey <stark3y@gmail.com>
2018-12-28 16:24:07 +0000

    Copy over skeleton from ui/

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"flag"
+	"image"
+	"time"
+
+	"github.com/ungerik/go-cairo"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+var bench bool
+
+func init() {
+	gob.Register(&image.NRGBA{})
+	gob.Register(&image.Gray{})
+
+	const (
+		defaultBench = false
+		usageBench   = "Measure drawing time"
+
+	)
+
+	flag.BoolVar(&bench, "b", defaultBench, usageBench)
+}
+
+func main() {
+	flag.Parse()
+
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	windowW := 1150
+	windowH := 600
+
+	window, err := sdl.CreateWindow("Mini Mouse", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		int32(windowW), int32(windowH), sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	sdlSurface, err := window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+
+	cairoSurface := cairo.NewSurfaceFromData(sdlSurface.Data(), cairo.FORMAT_ARGB32, int(sdlSurface.W), int(sdlSurface.H), int(sdlSurface.Pitch));
+
+
+	running := true
+	tick := time.NewTicker(16 * time.Millisecond)
+	for running {
+		<-tick.C
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch ev := event.(type) {
+			case *sdl.QuitEvent:
+				println("Quit")
+				running = false
+				break
+			case *sdl.KeyboardEvent:
+				_ = ev
+			}
+		}
+
+		now := time.Now()
+
+		// Finally draw to the screen
+		cairoSurface.Flush()
+		window.UpdateSurface()
+
+		if bench {
+			fmt.Printf("                              \r")
+			fmt.Printf("%v\r", time.Since(now))
+		}
+	}
+}
